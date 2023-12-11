07619f97a473b35a9b859e5204cc71fffe6f24b9
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-12-10 20:53:20 -0600

    corrected vite.config file typo

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/citp45316finalprojectfrontend/',
+  base: '/citp4316finalprojectfrontend/',
   plugins: [vue()],
   resolve: {
     alias: {
